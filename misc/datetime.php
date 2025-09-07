563780433072bf45a4d1998b29732ad445895ab6
reversejp <joaopedrosilvadelima@gmail.com>
2025-09-07 19:47:14 -0300

    Testing miscelanious concepts

---
+++ misc/datetime.php
@@ -0,0 +1,22 @@
+<?php
+$raw = '22. 11. 1968';
+$start = DateTime::createFromFormat('d. m. Y', $raw);
+
+echo 'Start date: ' . $start->format('Y-m-d') . PHP_EOL;
+
+$end = clone $start;
+$end->add(new DateInterval('P1M6D'));
+
+$diff = $end->diff($start);
+echo 'Difference: ' . $diff->format('%m month, %d days (total: %a days)') . PHP_EOL;
+
+if ($start < $end) {
+    echo "Start is before the end!" . PHP_EOL;
+}
+
+$periodInterval = DateInterval::createFromDateString('first thursday');
+$periodIterator = new DatePeriod($start, $periodInterval, $end, DatePeriod::EXCLUDE_START_DATE);
+foreach ($periodIterator as $date) {
+    // output each date in the period
+    echo $date->format('Y-m-d') . ' ';
+}
