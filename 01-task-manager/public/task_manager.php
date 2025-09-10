b773cc9ab5de0bcd9b1ca73f0f65909d7aa04977
reversejp <joaopedrosilvadelima@gmail.com>
2025-09-10 00:19:45 -0300

    Created little script with functionalities to create tasks and store it on a tasks file in the project folder.

--- 01-task-manager/public/task_manager.php
+++ 01-task-manager/public/task_manager.php
@@ -1,21 +1,74 @@
 <?php
 
-$TASKSFILE = "tasks.json";
+const TASKSFILE = "tasks.json";
 
-function setupTasks($filename)
+# Setting up tasks database file
+function setupTasks()
 {
-    if (!file_exists($filename)) {
-        $default_tasks = json_encode(["tasks" => []], JSON_PRETTY_PRINT);
-        if (file_put_contents($filename, $default_tasks) === false) {
-            throw new RuntimeException("Failed to create tasks file: $filename");
+    if (!file_exists(TASKSFILE)) {
+        $default_tasks = json_encode(
+            [
+                "tasks" => [],
+                "last_updated" => date('Y-m-d H:i:s')
+            ],
+            JSON_PRETTY_PRINT
+        );
+        if (file_put_contents(TASKSFILE, $default_tasks) === false) {
+            throw new RuntimeException("Failed to create tasks file: TASKSFILE");
         }
         return false; // new file created
     }
     return true; // file already existed
 }
 
-if (setupTasks($TASKSFILE)) {
-    echo "Found tasks file!";
+if (setupTasks()) {
+    echo "Found tasks file!" . PHP_EOL;
 } else {
-    echo "Tasks file not found. New one created!";
+    echo "Tasks file not found. New one created!" . PHP_EOL;
 }
+
+function getTasksFile()
+{
+    return json_decode(
+        file_get_contents(TASKSFILE),
+        true,
+    );
+}
+
+function saveTasksToFile($tasks)
+{
+    $tasksJSON = json_encode([
+        "tasks" => $tasks,
+        "last_updated" => date('Y-m-d H:i:s'),
+    ]);
+
+    if (file_put_contents(TASKSFILE, $tasksJSON) === false) {
+        throw new RuntimeException("Failed to store tasks file: TASKSFILE");
+    }
+
+    echo "task created!", PHP_EOL;
+}
+
+# Configuring CRUD operations for tasks
+
+# Task model:
+/* Task: {
+ *      "name": string,
+ *      "completed": bool,
+ * }
+ **/
+
+function createTask($task)
+{
+    $tasksFile = getTasksFile();
+    $tasks = $tasksFile['tasks'];
+    $tasks[] = $task;
+    saveTasksToFile($tasks);
+}
+
+$task = [
+    "nome" => "outro teste",
+    "completed" => true,
+];
+
+createTask($task);
